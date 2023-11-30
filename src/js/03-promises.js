ebdda6331ae6df69d08a01ab63d8a1002c5333dd
TZaidel <8tzaidel@gmail.com>
2023-11-30 19:53:35 +0100

    add notiflix library

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,27 +1,31 @@
 import Notiflix from 'notiflix';
 const form = document.querySelector('.form')
+const button = document.querySelector('.form button')
+
 form.addEventListener('submit', onSubmit)
 
 function onSubmit(event) {
   event.preventDefault()
-
+  button.setAttribute('disabled', '')//де цей атрибут можна видалити, щоб після закінчення - кнопка знову була актовною?
   const delay = +event.target.elements.delay.value 
   const step = +event.target.elements.step.value
   const amount = +event.target.elements.amount.value
-  console.log(delay, step, amount)
   
   for (let i = 0; i < amount; i++){
-    const promiseDelay = delay+step*i
-    console.log(promiseDelay)
+    const promiseDelay = delay + step * i
+    console.log(i+1, amount)
 
     createPromise(i + 1, promiseDelay)
       
-     .then(({ position, delay }) => {
-   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+    .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    })
+//         if (i+1 === amount) {
+//   button.removeAttribute('disabled', '')
+// }
   }
 }
 
