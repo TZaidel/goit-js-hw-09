9a041453e727c8eb6b676aa873cf850a33c8d504
TZaidel <8tzaidel@gmail.com>
2023-11-30 19:36:52 +0100

    edit task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,25 +1,26 @@
+import Notiflix from 'notiflix';
 const form = document.querySelector('.form')
 form.addEventListener('submit', onSubmit)
 
 function onSubmit(event) {
   event.preventDefault()
 
-  const delay = +event.target.elements.delay.value *1000
-  const step = +event.target.elements.step.value*1000
+  const delay = +event.target.elements.delay.value 
+  const step = +event.target.elements.step.value
   const amount = +event.target.elements.amount.value
   console.log(delay, step, amount)
   
   for (let i = 0; i < amount; i++){
-    const promiseDelay = delay+step
+    const promiseDelay = delay+step*i
     console.log(promiseDelay)
 
     createPromise(i + 1, promiseDelay)
       
      .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
   .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   });
   }
 }
@@ -38,4 +39,5 @@ function createPromise(position, delay) {
   })
 }
 
-//чи я в доброму напрямку рухаюсь? Здається залишилась тільки проблема з delay але я вже заплуталась. Можливо раніше не туди пішла
+
+
